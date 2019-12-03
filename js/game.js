e6e79cd4b33908ee35b0fab981d0d60070aaea6c
Rimantas <belauzas@yahoo.com>
2019-12-03 12:32:06 +0200

    Game init

--- js/game.js
+++ js/game.js
@@ -1,1 +1,35 @@
-// visa pagrindine zaidimo logika
+class DavGame {
+    constructor ( data ) {
+        this.DOM = document.querySelector(data.target);
+        this.player;
+        this.playerCar;
+        this.selected = 'car';      // 'player' || 'car'
+        this.playerSection = {
+            x: data.spawnPosition.x || 2,
+            y: data.spawnPosition.y || 2
+        }
+
+        this.init();
+    }
+
+    init() {
+        this.DOM.innerHTML = `
+            <div class="zoom">
+                <div class="map">
+                    ${this.renderMap()}
+                </div>
+                <div class="car car-black-1"></div>
+                <div class="person person-black-blue"></div>
+            </div>`;
+        this.DOM.classList.add('dav');
+
+        // uzkurti zaidimo varykli
+        // requestAnimationFrame()
+    }
+
+    renderMap() {
+        return 'ZEMELAPIS + REIKIAMOS SEKCIJOS';
+    }
+}
+
+export default DavGame;
--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+import DavGame from './game.js';
+
+const data = {
+    target: '#game',
+    spawnPosition: {
+        x: 2,
+        y: 2
+    }
+}
+
+const game = new DavGame( data );
+console.log(game);
